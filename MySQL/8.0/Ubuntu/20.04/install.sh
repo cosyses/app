46b1e02493097869b3dc7e1ea73f3b08058cb3d6
Andreas Knollmann <a.knollmann@seak.dev>
2023-08-16 15:01:36 +0200

    added MySQL 8.0

---
+++ MySQL/8.0/Ubuntu/20.04/install.sh
@@ -0,0 +1,127 @@
+#!/bin/bash -e
+
+scriptFileName="${BASH_SOURCE[0]}"
+if [[ -L "${scriptFileName}" ]] && [[ -x "$(command -v readlink)" ]]; then
+  scriptFileName=$(readlink -f "${scriptFileName}")
+fi
+
+usage()
+{
+cat >&2 << EOF
+
+usage: ${scriptFileName} options
+
+OPTIONS:
+  --help                  Show this message
+  --databaseRootHost      Server host name, default: localhost
+  --databaseRootPort      Server port, default: 3306
+  --databaseRootPassword  User password, default: <generated>
+  --bindAddress           Bind address, default: 0.0.0.0
+  --serverId              Server Id, default: 1
+
+Example: ${scriptFileName} --databaseRootPassword secret
+EOF
+}
+
+if [[ -z "${applicationName}" ]]; then
+  >&2 echo "No application name exported!"
+  echo ""
+  exit 1
+fi
+
+if [[ -z "${applicationVersion}" ]]; then
+  >&2 echo "No application version exported!"
+  echo ""
+  exit 1
+fi
+
+if [[ -z "${cosysesPath}" ]]; then
+  >&2 echo "No cosyses path exported!"
+  echo ""
+  exit 1
+fi
+
+databaseRootHost=
+databaseRootPort=
+databaseRootPassword=
+bindAddress=
+serverId=
+source "${cosysesPath}/prepare-parameters.sh"
+
+if [[ -z "${databaseRootHost}" ]]; then
+  databaseRootHost="localhost"
+fi
+
+if [[ -z "${databaseRootPort}" ]]; then
+  databaseRootPort="3306"
+fi
+
+if [[ -z "${databaseRootPassword}" ]]; then
+  databaseRootPassword=$(echo "${RANDOM}" | md5sum | head -c 32)
+  echo "Using generated password: ${databaseRootPassword}"
+fi
+
+if [[ -z "${bindAddress}" ]]; then
+  bindAddress="0.0.0.0"
+fi
+
+if [[ -z "${serverId}" ]]; then
+  serverId="1"
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Downloading libraries"
+install-package libaio1
+install-package libnuma1
+install-package libmecab2
+install-package libtinfo5
+install-package psmisc
+install-package-from-deb mysql-common 8.0.33-1ubuntu0.20.04 https://repo.mysql.com/apt/ubuntu/pool/mysql-8.0/m/mysql-community/mysql-common_8.0.33-1ubuntu20.04_amd64.deb
+install-package-from-deb mysql-community-client-plugins 8.0.33-1ubuntu0.20.04 https://repo.mysql.com/apt/ubuntu/pool/mysql-8.0/m/mysql-community/mysql-community-client-plugins_8.0.33-1ubuntu20.04_amd64.deb
+install-package-from-deb mysql-community-client-core 8.0.33-1ubuntu0.20.04 https://repo.mysql.com/apt/ubuntu/pool/mysql-8.0/m/mysql-community/mysql-community-client-core_8.0.33-1ubuntu20.04_amd64.deb
+install-package-from-deb mysql-community-client 8.0.33-1ubuntu0.20.04 https://repo.mysql.com/apt/ubuntu/pool/mysql-8.0/m/mysql-community/mysql-community-client_8.0.33-1ubuntu20.04_amd64.deb
+install-package-from-deb mysql-client 8.0.33-1ubuntu0.20.04  https://repo.mysql.com/apt/ubuntu/pool/mysql-8.0/m/mysql-community/mysql-client_8.0.33-1ubuntu20.04_amd64.deb
+install-package-from-deb mysql-community-server-core 8.0.33-1ubuntu0.20.04 https://repo.mysql.com/apt/ubuntu/pool/mysql-8.0/m/mysql-community/mysql-community-server-core_8.0.33-1ubuntu20.04_amd64.deb
+install-package-from-deb mysql-community-server 8.0.33-1ubuntu0.20.04  https://repo.mysql.com/apt/ubuntu/pool/mysql-8.0/m/mysql-community/mysql-community-server_8.0.33-1ubuntu20.04_amd64.deb
+
+if [[ -f /.dockerenv ]]; then
+  echo "Starting MySQL"
+  /usr/sbin/mysqld --daemonize --user mysql --pid-file=/var/run/mysqld/mysqld.pid
+fi
+
+cosyses \
+  --applicationName "${applicationName}" \
+  --applicationVersion "${applicationVersion}" \
+  --applicationScript "root-user.sh" \
+  --databaseRootPassword "${databaseRootPassword}"
+
+echo "Increasing file limits"
+add-file-content-before /etc/security/limits.conf "mysql  soft  nofile  65535" "# End of file" 1
+add-file-content-before /etc/security/limits.conf "mysql  hard  nofile  65535" "# End of file" 1
+sysctl -p
+
+if [[ -f /.dockerenv ]]; then
+  echo "Stopping MySQL"
+  kill "$(cat /var/run/mysqld/mysqld.pid)"
+
+  echo "Allowing binding from: ${bindAddress}"
+  sed -i "s/bind-address.*/bind-address = ${bindAddress}/g" /etc/mysql/mysql.conf.d/mysqld.cnf
+
+  echo "Creating start script at: /usr/local/bin/mysql.sh"
+  cat <<EOF > /usr/local/bin/mysql.sh
+#!/bin/bash -e
+/usr/sbin/mysqld --user mysql --pid-file=/var/run/mysqld/mysqld.pid
+EOF
+  chmod +x /usr/local/bin/mysql.sh
+else
+  echo "Restarting MySQL"
+  service mysql restart
+fi
+
+mkdir -p /opt/install/
+crudini --set /opt/install/env.properties mysql type "mysql"
+crudini --set /opt/install/env.properties mysql version "8.0"
+crudini --set /opt/install/env.properties mysql port "${databaseRootPort}"
+crudini --set /opt/install/env.properties mysql rootUser "root"
+crudini --set /opt/install/env.properties mysql rootPassword "${databaseRootPassword}"
--- prepare-parameters.sh
+++ prepare-parameters.sh
@@ -1,63 +1,75 @@
 #!/bin/bash -e
 
 declare -Ag prepareParameters
+prepareParametersList=()
+prepareParametersListParts=()
 unparsedParameters=( )
 while [[ "$#" -gt 0 ]]; do
   parameter="${1}"
   shift
-  if [[ "${parameter:0:2}" == "--" ]] || [[ "${parameter}" =~ ^-[[:alpha:]][[:space:]]+ ]] || [[ "${parameter}" =~ ^-\?$ ]]; then
-    if [[ "${parameter}" =~ ^--[[:alpha:]]+[[:space:]]+ ]]; then
-      parameter="${parameter:2}"
+  if [[ "${parameter}" =~ ^-[[:alpha:]]+ ]] || [[ "${parameter}" =~ ^--[[:alpha:]]+ ]] || [[ "${parameter}" =~ ^-[[:alpha:]][[:space:]]+ ]] || [[ "${parameter}" =~ ^--[[:alpha:]][[:space:]]+ ]] || [[ "${parameter}" =~ ^-\?$ ]]; then
+    if [[ "${parameter}" =~ ^-[[:alpha:]]+[[:space:]]+ ]] || [[ "${parameter}" =~ ^--[[:alpha:]]+[[:space:]]+ ]]; then
+      if [[ "${parameter}" =~ ^--[[:alpha:]]+[[:space:]]+ ]]; then
+        parameter="${parameter:2}"
+      elif [[ "${parameter}" =~ ^-[[:alpha:]]+[[:space:]]+ ]]; then
+        parameter="${parameter:1}"
+      fi
       prepareParametersKey=$(echo "${parameter}" | grep -oP '[[:alpha:]]+(?=\s)' | tr -d "\n")
       prepareParametersValue=$(echo "${parameter:${#prepareParametersKey}}" | xargs)
       # shellcheck disable=SC2034
       prepareParameters["${prepareParametersKey}"]="${prepareParametersValue}"
-      #echo eval "${prepareParametersKey}=\"${prepareParametersValue}\""
       eval "${prepareParametersKey}=\"${prepareParametersValue}\""
+      #echo eval "${prepareParametersKey}=\"${prepareParametersValue}\""
+      prepareParametersList+=( "--${prepareParametersKey} ${prepareParametersValue}" )
+      prepareParametersListParts+=( "--${prepareParametersKey}" )
+      prepareParametersListParts+=( "${prepareParametersValue}" )
       continue
     fi
+    skipParameter=0
     if [[ "${parameter:0:2}" == "--" ]]; then
       prepareParametersKey="${parameter:2}"
     elif [[ "${parameter}" =~ ^-\?$ ]]; then
       prepareParametersKey="help"
     else
       prepareParametersKey="${parameter:1}"
+      skipParameter=1
     fi
     if [[ "$#" -eq 0 ]]; then
-      prepareParameters["${prepareParametersKey}"]=1
-      #echo eval "${prepareParametersKey}=1"
-      eval "${prepareParametersKey}=1"
+      if [[ "${skipParameter}" == 0 ]]; then
+        prepareParameters["${prepareParametersKey}"]=1
+        eval "${prepareParametersKey}=1"
+        #echo eval "${prepareParametersKey}=1"
+        prepareParametersList+=( "--${prepareParametersKey}" )
+        prepareParametersListParts+=( "--${prepareParametersKey}" )
+        prepareParametersListParts+=( "1" )
+      fi
     else
       prepareParametersValue="${1}"
-      if [[ "${prepareParametersValue:0:2}" == "--" ]]; then
-        prepareParameters["${prepareParametersKey}"]=1
-        #echo eval "${prepareParametersKey}=1"
-        eval "${prepareParametersKey}=1"
+      if [[ "${prepareParametersValue}" =~ ^-[[:alpha:]]+ ]] || [[ "${prepareParametersValue}" =~ ^--[[:alpha:]]+ ]]; then
+        if [[ "${skipParameter}" == 0 ]]; then
+          prepareParameters["${prepareParametersKey}"]=1
+          eval "${prepareParametersKey}=1"
+          #echo eval "${prepareParametersKey}=1"
+          prepareParametersList+=( "--${prepareParametersKey}" )
+          prepareParametersListParts+=( "--${prepareParametersKey}" )
+          prepareParametersListParts+=( "1" )
+        fi
         continue
       fi
       shift
       # shellcheck disable=SC2034
       prepareParameters["${prepareParametersKey}"]="${prepareParametersValue}"
-      #echo eval "${prepareParametersKey}=\"${prepareParametersValue}\""
       eval "${prepareParametersKey}=\"${prepareParametersValue}\""
+      #echo eval "${prepareParametersKey}=\"${prepareParametersValue}\""
+      prepareParametersList+=( "--${prepareParametersKey} ${prepareParametersValue}" )
+      prepareParametersListParts+=( "--${prepareParametersKey}" )
+      prepareParametersListParts+=( "${prepareParametersValue}" )
     fi
   else
     unparsedParameters+=("${parameter}")
   fi
 done
 set -- "${unparsedParameters[@]}"
-
-prepareParametersList=()
-for prepareParametersKey in "${!prepareParameters[@]}"; do
-  prepareParametersList+=( "--${prepareParametersKey}" )
-  prepareParametersValue="${prepareParameters[${prepareParametersKey}]}"
-  prepareParametersList+=( "${prepareParametersValue}" )
-done
-for unparsedParametersKey in "${!unparsedParameters[@]}"; do
-  unparsedParametersValue="${unparsedParameters[${unparsedParametersKey}]}"
-  prepareParametersList+=( "${unparsedParametersValue}" )
-done
-export prepareParametersList
 
 if test "${prepareParameters["help"]+isset}" || test "${prepareParameters["?"]+isset}"; then
   helpRequested=1
