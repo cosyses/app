2c90b5964e6383e17e654b927ef7264599e411cf
Andreas Knollmann <a.knollmann@seak.dev>
2023-09-05 07:43:31 +0200

    added MongoDB 5.0

---
+++ MongoDB/5.0/Ubuntu/22.04/install.sh
@@ -0,0 +1,108 @@
+#!/bin/bash -e
+
+scriptFileName="${BASH_SOURCE[0]}"
+if [[ -L "${scriptFileName}" ]] && [[ -x "$(command -v readlink)" ]]; then
+  scriptFileName=$(readlink -f "${scriptFileName}")
+fi
+
+usage()
+{
+cat >&2 << EOF
+
+usage: ${scriptFileName} options
+
+OPTIONS:
+  --help           Show this message
+  --bindAddress    Bind address, default: 127.0.0.1
+  --port           Server port, default: 27017
+  --adminUserName  Name of admin user, default: admin
+  --adminPassword  Password of admin user, default: <generated>
+
+Example: ${scriptFileName} --adminPassword secret
+EOF
+}
+
+if [[ -z "${cosysesPath}" ]]; then
+  >&2 echo "No cosyses path exported!"
+  echo ""
+  exit 1
+fi
+
+bindAddress=
+port=
+adminUserName=
+adminPassword=
+source "${cosysesPath}/prepare-parameters.sh"
+
+if [[ -z "${bindAddress}" ]]; then
+  bindAddress="0.0.0.0"
+fi
+
+if [[ -z "${port}" ]]; then
+  port="27017"
+fi
+
+if [[ -z "${adminUserName}" ]]; then
+  adminUserName="admin"
+fi
+
+if [[ -z "${adminPassword}" ]]; then
+  adminPassword=$(echo "${RANDOM}" | md5sum | head -c 32)
+  echo "Using generated password: ${adminPassword}"
+fi
+
+add-repository mongodb.list https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse https://www.mongodb.org/static/pgp/server-5.0.asc
+
+install-package-from-deb libssl1.1 1.1.1f-1ubuntu2 http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+
+install-package mongodb-org 5.0.20
+
+echo "Allowing binding from: ${bindAddress}"
+replace-file-content /etc/mongod.conf "bindIp: ${bindAddress}" "bindIp: 127.0.0.1"
+
+echo "Using port: ${port}"
+replace-file-content /etc/mongod.conf "port: ${port}" "port: 27017"
+
+replace-file-content /etc/mongod.conf "security:" "#security:" 0
+add-file-content-after /etc/mongod.conf "  authorization: enabled" "security:" 1
+
+echo "Starting MongoDB"
+if [[ -f /.dockerenv ]]; then
+  install-package sudo
+
+  mkdir -p /var/run/mongod && chown mongodb:mongodb /var/run/mongod
+  sudo -H -u mongodb bash -c "/usr/bin/mongod -f /etc/mongod.conf --pidfilepath /var/run/mongod/mongodb.pid --fork"
+else
+  systemctl start mongod
+fi
+
+mongosh <<EOF
+use admin;
+db.createUser({
+user: "${adminUserName}",
+pwd: "${adminPassword}",
+roles: [
+       { role: "userAdminAnyDatabase", db: "admin" },
+       { role: "readWriteAnyDatabase", db: "admin" }
+     ]
+})
+EOF
+
+if [[ -f /.dockerenv ]]; then
+  echo "Stopping MongoDB"
+  kill "$(cat /var/run/mongod/mongodb.pid)"
+
+  echo "Creating start script at: /usr/local/bin/mongodb.sh"
+  cat <<EOF > /usr/local/bin/mongodb.sh
+#!/bin/bash -e
+mkdir -p /var/run/mongod && chown mongodb:mongodb /var/run/mongod
+sudo -H -u mongodb bash -c "/usr/bin/mongod -f /etc/mongod.conf --pidfilepath /var/run/mongod/mongodb.pid"
+EOF
+  chmod +x /usr/local/bin/mongodb.sh
+fi
+
+mkdir -p /opt/install/
+crudini --set /opt/install/env.properties mongodb version "5.0"
+crudini --set /opt/install/env.properties mongodb port "${port}"
+crudini --set /opt/install/env.properties mongodb rootUser "${adminUserName}"
+crudini --set /opt/install/env.properties mongodb rootPassword "${adminPassword}"
