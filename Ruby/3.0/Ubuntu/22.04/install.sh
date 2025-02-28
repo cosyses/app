9286d86c65276524a75124d9d8054564fa51c37e
Andreas Knollmann <a.knollmann@seak.dev>
2025-02-28 16:41:31 +0100

    update Ruby docker script

--- Ruby/3.0/Ubuntu/22.04/install.sh
+++ Ruby/3.0/Ubuntu/22.04/install.sh
@@ -6,8 +6,18 @@ install-package ruby-dev "1:3.0"
 if [[ -f /.dockerenv ]]; then
   echo "Creating start script at: /usr/local/bin/ruby.sh"
   cat <<EOF > /usr/local/bin/ruby.sh
-#!/bin/bash -e
-tail -f /dev/null
+#!/usr/bin/env bash
+trap stop SIGTERM SIGINT SIGQUIT SIGHUP ERR
+stop() {
+  echo "Stopping Ruby"
+  exit
+}
+for command in "\$@"; do
+  echo "Run: \${command}"
+  /bin/bash "\${command}"
+done
+echo "Starting Ruby"
+tail -f /dev/null & wait \$!
 EOF
   chmod +x /usr/local/bin/ruby.sh
 fi
