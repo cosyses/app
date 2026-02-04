4b48e77b51ee08a00ff60f4f33ae516bbf34e21b
Andreas Knollmann <a.knollmann@seak.dev>
2026-02-04 19:54:12 +0100

    Update RabbitMQ

--- RabbitMQ/3.11/Ubuntu/22.04/install.sh
+++ RabbitMQ/3.11/Ubuntu/22.04/install.sh
@@ -74,11 +74,21 @@ if [[ -z "${adminUserName}" ]]; then
   adminUserName="admin"
 fi
 
+install-package locales
+
+{ echo "LC_ALL=en_US.UTF-8"; echo "LANG=en_US.UTF-8"; echo "LANGUAGE=en_US.UTF-8"; } >> /etc/environment
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+locale-gen en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
 add-server-key-id "hkps://keys.openpgp.org" "0x0A9AF2115F4687BD29803A206B73A36E6026DFCA"
 add-server-key-id "keyserver.ubuntu.com" "F77F1EDA57EBB1CC"
 
-add-gpg-repository "erlang.list" "https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/deb/ubuntu" "bionic" "main" "https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/gpg.E495BB49CC4BBE5B.key" "y"
-add-gpg-repository "rabbitmq.list" "https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/deb/ubuntu" "bionic" "main" "https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/gpg.9F4587F226208342.key" "y"
+add-gpg-repository "erlang.list" "https://deb1.rabbitmq.com/rabbitmq-erlang/ubuntu/jammy" "jammy" "main" "https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA" "n"
+add-gpg-repository "rabbitmq.list" "https://deb1.rabbitmq.com/rabbitmq-server/ubuntu/jammy" "jammy" "main" "https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA" "n"
 
 add-repository "focal-security.list" "http://security.ubuntu.com/ubuntu" "focal-security" "main"
 
@@ -87,14 +97,22 @@ install-package libssl1.1
 
 ## Install Erlang packages
 install-package erlang-base 1:25.2.3
+install-package erlang-asn1 1:25.2.3
+install-package erlang-mnesia 1:25.2.3
 install-package erlang-crypto 1:25.2.3
+install-package erlang-public-key 1:25.2.3
+install-package erlang-runtime-tools 1:25.2.3
+install-package erlang-ssl 1:25.2.3
 install-package erlang-eldap 1:25.2.3
+install-package erlang-ftp 1:25.2.3
+install-package erlang-tftp 1:25.2.3
 install-package erlang-inets 1:25.2.3
+install-package erlang-snmp 1:25.2.3
 install-package erlang-os-mon 1:25.2.3
 install-package erlang-parsetools 1:25.2.3
-install-package erlang-public-key 1:25.2.3
 install-package erlang-tools 1:25.2.3
 install-package erlang-xmerl 1:25.2.3
+install-package erlang-syntax-tools 1:25.2.3
 
 ## Install rabbitmq-server and its dependencies
 install-package rabbitmq-server 3.11
