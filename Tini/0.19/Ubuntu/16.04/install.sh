d6aa54d1dafa8920e97c3ca6cb0a5bffe07851fc
Andreas Knollmann <a.knollmann@seak.dev>
2026-02-10 14:13:24 +0100

    Update Tini

--- Tini/0.19/Ubuntu/16.04/install.sh
+++ Tini/0.19/Ubuntu/16.04/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 echo "Installing Tini"
-wget -nv https://github.com/krallin/tini/releases/download/v0.19.0/tini -O /usr/local/bin/tini
+wget -nv https://github.com/krallin/tini/releases/download/v0.19.0/tini -O /usr/local/bin/tini 2>&1
 chmod +x /usr/local/bin/tini
