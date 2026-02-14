0688edc125c48e81f9199e3182b5ac2bad941278
Andreas Knollmann <a.knollmann@seak.dev>
2026-02-14 20:02:00 +0100

    Update RabbitMQ

--- RabbitMQ/create-user.sh
+++ RabbitMQ/create-user.sh
@@ -12,12 +12,12 @@ cat >&2 << EOF
 usage: ${scriptFileName} options
 
 OPTIONS:
-  --help                 Show this message
-  --rabbitMqUser         RabbitMQ user
-  --rabbitMqPassword     RabbitMQ password
-  --rabbitMqVirtualHost  RabbitMQ virtual host, default: /
+  --help              Show this message
+  --queueUser         RabbitMQ user
+  --queuePassword     RabbitMQ password
+  --queueVirtualHost  RabbitMQ virtual host, default: /
 
-Example: ${scriptFileName} --rabbitMqUser newuser --rabbitMqPassword password --rabbitMqVirtualHost /
+Example: ${scriptFileName} --queueUser newuser --queuePassword password --queueVirtualHost /
 EOF
 }
 
@@ -27,45 +27,45 @@ if [[ -z "${cosysesPath}" ]]; then
   exit 1
 fi
 
-rabbitMqUser=
-rabbitMqPassword=
-rabbitMqVirtualHost=
+queueUser=
+queuePassword=
+queueVirtualHost=
 source "${cosysesPath}/prepare-parameters.sh"
 
-if [[ -z "${rabbitMqUser}" ]]; then
+if [[ -z "${queueUser}" ]]; then
   echo "No RabbitMQ user specified!"
   usage
   exit 1
 fi
 
-if [[ -z "${rabbitMqPassword}" ]]; then
+if [[ -z "${queuePassword}" ]]; then
   echo "No RabbitMQ password specified!"
   usage
   exit 1
 fi
 
-if [[ -z "${rabbitMqVirtualHost}" ]]; then
-  rabbitMqVirtualHost="/"
+if [[ -z "${queueVirtualHost}" ]]; then
+  queueVirtualHost="/"
 fi
 
-if [[ -n "${rabbitMqUser}" ]] && [[ $(rabbitmqctl list_users 2>/dev/null | tail -n +3 | awk '{print $1}' | grep -P "^${rabbitMqUser}$" | wc -l) -eq 0 ]]; then
-  echo "Adding user: ${rabbitMqUser}"
-  rabbitmqctl add_user "${rabbitMqUser}" "${rabbitMqPassword}"
-  rabbitmqctl set_user_tags "${rabbitMqUser}" management
+if [[ -n "${queueUser}" ]] && [[ $(rabbitmqctl list_users 2>/dev/null | tail -n +3 | awk '{print $1}' | grep -P "^${queueUser}$" | wc -l) -eq 0 ]]; then
+  echo "Adding user: ${queueUser}"
+  rabbitmqctl add_user "${queueUser}" "${queuePassword}"
+  rabbitmqctl set_user_tags "${queueUser}" management
 else
-  echo "User: ${rabbitMqUser} already exists"
+  echo "User: ${queueUser} already exists"
 fi
 
-if [[ -n "${rabbitMqVirtualHost}" ]] && [[ $(rabbitmqctl list_vhosts 2>/dev/null | tail -n +3 | awk '{print $1}' | grep "${rabbitMqVirtualHost}" | wc -l) -eq 0 ]]; then
-  echo "Adding virtual host: ${rabbitMqVirtualHost}"
-  rabbitmqctl add_vhost "${rabbitMqVirtualHost}"
+if [[ -n "${queueVirtualHost}" ]] && [[ $(rabbitmqctl list_vhosts 2>/dev/null | tail -n +3 | awk '{print $1}' | grep "${queueVirtualHost}" | wc -l) -eq 0 ]]; then
+  echo "Adding virtual host: ${queueVirtualHost}"
+  rabbitmqctl add_vhost "${queueVirtualHost}"
 else
-  echo "Virtual host: ${rabbitMqVirtualHost} already exists"
+  echo "Virtual host: ${queueVirtualHost} already exists"
 fi
 
-if [[ -n "${rabbitMqUser}" ]] && [[ -n "${rabbitMqVirtualHost}" ]] && [[ $(rabbitmqctl list_user_permissions "${rabbitMqUser}" 2>/dev/null | tail -n +3 | awk '{print $1}' | grep -P "^${rabbitMqVirtualHost}$" | wc -l) -eq 0 ]]; then
-  echo "Adding permission for user: ${rabbitMqUser} to virtual host: ${rabbitMqVirtualHost}"
-  rabbitmqctl set_permissions -p "${rabbitMqVirtualHost}" "${rabbitMqUser}" ".*" ".*" ".*"
+if [[ -n "${queueUser}" ]] && [[ -n "${queueVirtualHost}" ]] && [[ $(rabbitmqctl list_user_permissions "${queueUser}" 2>/dev/null | tail -n +3 | awk '{print $1}' | grep -P "^${queueVirtualHost}$" | wc -l) -eq 0 ]]; then
+  echo "Adding permission for user: ${queueUser} to virtual host: ${queueVirtualHost}"
+  rabbitmqctl set_permissions -p "${queueVirtualHost}" "${queueUser}" ".*" ".*" ".*"
 else
-  echo "User: ${rabbitMqUser} already has permission to virtual host: ${rabbitMqVirtualHost}"
+  echo "User: ${queueUser} already has permission to virtual host: ${queueVirtualHost}"
 fi
