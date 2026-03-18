bf6d8253696711a5e622631075f2feaa677eddaf
Andreas Knollmann <a.knollmann@seak.dev>
2026-03-18 16:02:56 +0100

    Update Composer installation

--- Composer/2.3/install.sh
+++ Composer/2.3/install.sh
@@ -2,4 +2,7 @@
 
 install-package curl
 
+phpVersion=$(php -v 2>/dev/null | grep --only-matching --perl-regexp "(PHP )\d+\.\\d+\.\\d+" | cut -c 5-7)
+install-package "php${phpVersion}-curl"
+
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --version 2.3.10
