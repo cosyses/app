e1d5bffdfb12c816f362e57e979459b85609564f
Andreas Knollmann <a.knollmann@seak.dev>
2026-04-06 13:40:25 +0200

    Update Laravel PHP installation

--- PHP/8.4/Ubuntu/22.04/laravel.sh
+++ PHP/8.4/Ubuntu/22.04/laravel.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -e
 
+if [[ -z "${applicationName}" ]]; then
+  >&2 echo "No application name exported!"
+  echo ""
+  exit 1
+fi
+
+if [[ -z "${applicationVersion}" ]]; then
+  >&2 echo "No application version exported!"
+  echo ""
+  exit 1
+fi
+
 echo "Checking curl"
 test ! -e /usr/local/include/curl && ln -s /usr/include/x86_64-linux-gnu/curl /usr/local/include/curl || echo "Successful"
 
@@ -8,6 +20,11 @@ install-package php8.4-mbstring
 install-package php8.4-mysql
 install-package php8.4-sqlite3
 install-package php8.4-xml
+
+cosyses \
+  --applicationName "${applicationName}" \
+  --applicationVersion "${applicationVersion}" \
+  --applicationScript module/redis.sh
 
 if [[ ! -f /.dockerenv ]]; then
   if [[ $(get-installed-package-version apache2 | wc -l) -gt 0 ]]; then
