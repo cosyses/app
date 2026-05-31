72fb07dbaea55b2440daf790ee4a6b9b90b9072a
Andreas Knollmann <a.knollmann@seak.dev>
2026-05-31 15:35:12 +0200

    Added PHP 8.4 Symfony Installation

---
+++ PHP/8.4/Ubuntu/22.04/symfony.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+if [[ -z "${applicationName}" ]]; then
+  >&2 echo "No application name exported!"
+  echo ""
+  exit 1
+fi
+
+if [[ -z "${applicationVersion}" ]]; then
+  >&2 echo "No application version exported!"
+  echo ""
+  exit 1
+fi
+
+echo "Checking curl"
+test ! -e /usr/local/include/curl && ln -s /usr/include/x86_64-linux-gnu/curl /usr/local/include/curl || echo "Successful"
+
+install-package php8.4-curl
+install-package php8.4-mbstring
+install-package php8.4-intl
+install-package php8.4-mysql
+
+cosyses \
+  --applicationName "${applicationName}" \
+  --applicationVersion "${applicationVersion}" \
+  --applicationScript module/redis.sh
+
+if [[ ! -f /.dockerenv ]]; then
+  if [[ $(get-installed-package-version apache2 | wc -l) -gt 0 ]]; then
+    service apache2 restart
+    sleep 5
+  fi
+
+  if [[ $(get-installed-package-version nginx | wc -l) -gt 0 ]]; then
+    service nginx restart
+  fi
+fi
