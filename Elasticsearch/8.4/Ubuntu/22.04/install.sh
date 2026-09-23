7c2e73552a9d94ed96a31a8e6ff45a9ff2406e09
Andreas Knollmann <a.knollmann@seak.dev>
2026-09-23 15:41:19 +0200

    Added Elasticsearch 8.4

---
+++ Elasticsearch/8.4/Ubuntu/22.04/install.sh
@@ -0,0 +1,118 @@
+#!/bin/bash -e
+
+currentPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+scriptName="${0##*/}"
+
+usage()
+{
+cat >&2 << EOF
+usage: ${scriptName} options
+
+OPTIONS:
+  --help                Show this message
+  --applicationName     Name of application
+  --applicationVersion  Version of application (optional)
+  --applicationScript   Name of script, default: install.sh
+  --bindAddress         Bind address, default: 127.0.0.1
+  --port                Port, default: 9200
+
+Example: ${scriptName} --applicationName Elasticsearch --applicationVersion 8.4 --bindAddress 0.0.0.0 --port 9200
+EOF
+}
+
+helpRequested=
+bindAddress=
+port=
+source "${currentPath}/../../../../prepare-parameters.sh"
+
+if [[ "${helpRequested}" == 1 ]]; then
+  usage
+  exit 0
+fi
+
+if [[ -z "${bindAddress}" ]]; then
+  bindAddress="127.0.0.1"
+fi
+
+if [[ -z "${port}" ]] || [[ "${port}" == "-" ]]; then
+  port="9200"
+fi
+
+echo "Installing Java 8"
+install-package python3-software-properties
+install-package debconf-utils
+install-package openjdk-8-jre
+
+add-asc-repository "elastic-8.x.list" "https://artifacts.elastic.co/packages/8.x/apt" "stable" "main" "https://artifacts.elastic.co/GPG-KEY-elasticsearch" "n"
+
+echo "Installing Elasticsearch 8.4"
+install-package elasticsearch 8.4
+
+sysctl -w vm.max_map_count=262144
+
+echo "Setting bind address to: ${bindAddress}"
+replace-file-content /etc/elasticsearch/elasticsearch.yml "network.host: ${bindAddress}" "#network.host: 192.168.0.1" 0
+
+echo "Setting port to: ${port}"
+replace-file-content /etc/elasticsearch/elasticsearch.yml "http.port: ${port}" "#http.port: 9200" 0
+
+echo "Disabling security"
+replace-file-content /etc/elasticsearch/elasticsearch.yml "xpack.security.enabled: false" "xpack.security.enabled: true"
+
+if [[ -f /.dockerenv ]]; then
+  echo "Removing service"
+  systemctl disable elasticsearch.service
+
+  echo "Creating start script at: /usr/local/bin/elasticsearch.sh"
+  cat <<EOF > /usr/local/bin/elasticsearch.sh
+#!/usr/bin/env bash
+trap stop SIGTERM SIGINT SIGQUIT SIGHUP ERR
+stop() {
+  echo "Stopping Elasticsearch"
+  cat /var/run/elasticsearch/elasticsearch.pid | xargs kill -15
+  exit
+}
+for command in "\$@"; do
+  echo "Run: \${command}"
+  /bin/bash "\${command}"
+done
+echo "Starting Elasticsearch"
+ulimit -n 65535
+sysctl -w vm.max_map_count=262144
+mkdir -p /var/run/elasticsearch/
+chown elasticsearch: /var/run/elasticsearch/
+sudo -H -u elasticsearch bash -c "/usr/share/elasticsearch/bin/elasticsearch --pidfile /var/run/elasticsearch/elasticsearch.pid --daemonize" &
+tail -f /dev/null & wait \$!
+EOF
+  chmod +x /usr/local/bin/elasticsearch.sh
+
+  if [[ -d /usr/local/lib/start/ ]]; then
+    echo "Creating start script at: /usr/local/lib/start/10-elasticsearch.sh"
+    cat <<EOF > /usr/local/lib/start/10-elasticsearch.sh
+#!/usr/bin/env bash
+echo "Starting Elasticsearch"
+ulimit -n 65535
+sysctl -w vm.max_map_count=262144
+mkdir -p /var/run/elasticsearch/
+chown elasticsearch: /var/run/elasticsearch/
+sudo -H -u elasticsearch bash -c "/usr/share/elasticsearch/bin/elasticsearch --pidfile /var/run/elasticsearch/elasticsearch.pid --daemonize"
+EOF
+    chmod +x /usr/local/lib/start/10-elasticsearch.sh
+  fi
+
+  if [[ -d /usr/local/lib/stop/ ]]; then
+    echo "Creating stop script at: /usr/local/lib/stop/10-elasticsearch.sh"
+    cat <<EOF > /usr/local/lib/stop/10-elasticsearch.sh
+#!/usr/bin/env bash
+echo "Stopping Elasticsearch"
+cat /var/run/elasticsearch/elasticsearch.pid | xargs kill -15 && until test ! -f /var/run/elasticsearch/elasticsearch.pid; do sleep 1; done
+EOF
+    chmod +x /usr/local/lib/stop/10-elasticsearch.sh
+  fi
+else
+  echo "Restarting Elasticsearch"
+  service elasticsearch restart
+
+  echo "Adding autostart"
+  systemctl enable elasticsearch.service
+fi
